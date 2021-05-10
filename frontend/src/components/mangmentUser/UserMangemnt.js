19f216e0351a6bda7b1b93fe33321bcc614cb35d
KS-coder-98 <krzysztofsekowski1998@gmail.com>
2021-05-10 21:11:20 +0200

    display all users in panel admin

--- frontend/src/components/mangmentUser/UserMangemnt.js
+++ frontend/src/components/mangmentUser/UserMangemnt.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { ListGroup, Col, Row, Button } from "react-bootstrap";
 import { getAllUsers } from "../../helpers/UserHelper";
 
 const UserMangemnt = () => {
@@ -15,7 +16,25 @@ const UserMangemnt = () => {
     fetchData();
   }, []);
 
-  return <div>qwer</div>;
+  return (
+    <>
+      <ListGroup className="my-2" horizontal={2}>
+        {users.map((user) => (
+          <ListGroup.Item key={user.id}>
+            <Row>
+              <Col>{user.id}</Col>
+              <Col>{user.firstName}</Col>
+              <Col>{user.email}</Col>
+              <Col>{user.userRole}</Col>
+              <Col>
+                <Button>Edytuj</Button>{" "}
+              </Col>
+            </Row>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </>
+  );
 };
 
 export default UserMangemnt;
