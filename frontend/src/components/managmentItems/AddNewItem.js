32b56275996f9dcf4d59cc48fee05e6896c053f8
KS-coder-98 <krzysztofsekowski1998@gmail.com>
2021-05-11 21:20:52 +0200

    add seter category to add new item

--- frontend/src/components/managmentItems/AddNewItem.js
+++ frontend/src/components/managmentItems/AddNewItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Button,
   Form,
@@ -9,11 +9,23 @@ import {
 import { addItem } from "../../helpers/ItemHelper";
 
 const AddNewItem = () => {
-  //   const [item, setItem] = useState({});
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [unit, setUnit] = useState("");
+  const [categorySelected, setCategorySelected] = useState({});
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    async function getCategories() {
+      const response = await fetch("http://localhost:8080/api/categories");
+      response.json().then((t) => {
+        for (let i = 0; i < t.length; i++) {}
+        setCategories(t);
+      });
+    }
+    getCategories();
+  }, []);
 
   const DAY = 1;
   const HOUR = 2;
@@ -24,6 +36,13 @@ const AddNewItem = () => {
     } else {
       setUnit("HOUR");
     }
+  };
+
+  const handleChangeCategory = (val) => {
+    let temp = categories.filter((categry) => {
+      return categry.id === val;
+    });
+    setCategorySelected(temp);
   };
 
   const getNameFromInput = (input) => {
@@ -38,8 +57,17 @@ const AddNewItem = () => {
     setPrice(input.target.value);
   };
 
+  const checkForm = () => {
+    if (name === "") return false;
+    if (description === "") return false;
+    if (!isNaN(price)) return false;
+    if (unit === "") return false;
+    if (categorySelected.length === 0) return false;
+    return true;
+  };
+
   return (
-    <Container className="formContainer">
+    <Container>
       <Form>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Nazwa przedmiotu</Form.Label>
@@ -60,14 +88,28 @@ const AddNewItem = () => {
             type="radio"
             value={"DAY"}
             onChange={handleChange}
-            // defaultValue={item.unit === "DAY" ? DAY : HOUR}
           >
             <ToggleButton value={DAY}>Dzień</ToggleButton>
             <ToggleButton value={HOUR}>Godzina</ToggleButton>
           </ToggleButtonGroup>
         </Form.Group>
+        <Form.Group controlId="formCategories">
+          <Form.Label>Wybierz kategorie</Form.Label>
+          <ToggleButtonGroup
+            name="categories"
+            type="radio"
+            onChange={handleChangeCategory}
+          >
+            {categories.map((category) => (
+              <ToggleButton key={category.id} value={category.id}>
+                {category.itemTypes}
+              </ToggleButton>
+            ))}
+          </ToggleButtonGroup>
+        </Form.Group>
       </Form>
       <Button
+        disabled={checkForm()}
         onClick={async () => {
           const temp = await addItem({
             name: name,
@@ -76,6 +118,7 @@ const AddNewItem = () => {
             unit: unit,
             available: true,
             itemCorrect: true,
+            itemCategorySet: categorySelected,
           });
           alert(temp.status);
         }}
